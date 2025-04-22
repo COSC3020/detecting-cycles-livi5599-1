ba2d8eba6eafbb4727194f342b31afababb96718
livi5599 <obrown1@uwyo.edu>
2025-04-21 21:06:15 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,29 @@
 function hasCycle(graph) {
+    let queue = [];
+    let visited = [];
+    let current = 0;
+    let parent = null;
+    visited.push(current);
+    queue.push(current);
+
+    while (queue.length != 0) {
+        current = queue.shift();
+        for (let neighbor = 0; neighbor < graph[current].length; neighbor++) {
+            if (graph[current][neighbor] === 0) {
+                continue;
+            }
+            else if (neighbor === parent) {
+                continue;
+            }
+            else if (visited.includes(neighbor)) {
+                return true;
+            }
+            else {
+                visited.push(neighbor);
+                queue.push(neighbor);
+            }
+        }
+        parent = current;
+    }
     return false;
 }
